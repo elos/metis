1a4a03538ff546d2f7bcf0b156d028a52931fc3c
Nick Landolfi <nclandolfi@gmail.com>
2015-05-15 20:00:18 -0700

    add docs to funcs

--- funcs.go
+++ funcs.go
@@ -2,11 +2,14 @@ package metis
 
 import "strings"
 
+//  SplitSnakeCase splits a string based on the "_" character
+//  e.g., this_is_snake => []string{"this", "is", "snake"}
 func SplitSnakeCase(s string) []string {
 	return strings.Split(s, "_")
 }
 
-// this_is_camel => thisIsCamel
+// CamelCase turns a string from snake_case to camelCase
+// e.g., this_is_camel => thisIsCamel
 func CamelCase(s string) string {
 	splits := SplitSnakeCase(s)
 	ns := ""
@@ -21,6 +24,11 @@ func CamelCase(s string) string {
 	return ns
 }
 
+// Initials returns the first letter of each word
+// as defined by snake case.
+// e.g., this_is_snake_case => []string{"t", "i", "s", "c"}
+// Used at times to define the variable used as a struct pointer for
+// methods in go i.e., u for User model
 func Initials(s string) []string {
 	splits := SplitSnakeCase(s)
 	for i, s := range splits {
@@ -29,6 +37,8 @@ func Initials(s string) []string {
 	return splits
 }
 
+// AppendStrings joins a list of strings together
+// useful for templates {{append metis.Name metis.LinkName}}
 func AppendStrings(v ...string) {
 	n := ""
 	for _, s := range v {
@@ -36,10 +46,14 @@ func AppendStrings(v ...string) {
 	}
 }
 
-func IsMul(r *Link) bool {
-	return r.Multiplicity == Mul
+// IsMul returns a flag whether the link's
+// multiplicity == metis.Mul
+func IsMul(l *Link) bool {
+	return l.Multiplicity == Mul
 }
 
+// IsID returns a flag whether the trait's
+// primitive type == metis.ID
 func IsID(t *Trait) bool {
 	return t.Type == ID
 }
