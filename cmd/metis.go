2624e5f8b66637ca34ca78d1aed6cb70b83d8505
Nick Landolfi <nclandolfi@gmail.com>
2015-08-29 20:42:51 -0700

    clean up error reporting

--- cmd/metis.go
+++ cmd/metis.go
@@ -8,31 +8,38 @@ import (
 	"github.com/elos/metis"
 )
 
+// Note: the first arg is _always_ "metis"
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatal("please provide a file name, i.e., metis model.json or metis \"*.json\"")
+		log.Fatal("Please provide a file name, i.e., metis model.json or metis \"*.json\"")
 	}
 
 	arg := os.Args[1]
 	models := make([]*metis.Model, 1)
-	if strings.Contains(arg, "*") {
+
+	if strings.Contains(arg, "*") { // glob
 		var err error
 		models, err = metis.ParseGlob(arg)
+
 		if err != nil {
-			log.Fatalf("error parsing %s: %s", arg, err)
+			log.Fatalf("Error parsing %s: %s", arg, err)
 		}
+
 	} else {
 		model, err := metis.ParseModelFile(arg)
+
+		// Print the Error
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error parsing %s: %s", arg, err)
 		}
+
 		models[0] = model
 	}
 
 	s := metis.BuildSchema(models...)
 	if err := s.Valid(); err != nil {
-		log.Fatal(err)
+		log.Fatal("Error validating schema: %s", err)
 	}
 
-	log.Print("schema valid")
+	log.Print("Schema Valid")
 }
